4ae2d2236f15c25831740c29c2d5d7c053b3c88d
Ifra Ahmed <ifraahmed817@gmail.com>
2026-04-03 01:33:25 +0500

    added services

--- lib/services/medicine_service.dart
+++ lib/services/medicine_service.dart
@@ -18,4 +18,29 @@ class MedicineService {
   Future<void> addMedicine(Medicine medicine) async {
     await _supabase.client.from('medicines').insert(medicine.toJson());
   }
+
+  Future<void> updateMedicine(String medicineId, Medicine medicine) async {
+    await _supabase.client
+        .from('medicines')
+        .update(medicine.toJson())
+        .eq('medicine_id', medicineId);
+  }
+
+  Future<void> deleteMedicine(String medicineId) async {
+    await _supabase.client
+        .from('medicines')
+        .delete()
+        .eq('medicine_id', medicineId);
+  }
+
+  Future<int> getMedicinesCount() async {
+    try {
+      final res = await _supabase.client
+          .from('medicines')
+          .select('medicine_id');
+      return (res as List).length;
+    } catch (e) {
+      throw Exception('Failed to get medicines count: $e');
+    }
+  }
 }
